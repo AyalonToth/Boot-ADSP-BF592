f552283699b67c06d5a1677cd919902424bede22
AyalonToth <94567909+AyalonToth@users.noreply.github.com>
2022-05-18 10:24:30 -0300

    Arquivos .bat
    Inserido os arquivos .bat para gravar na memoria SPI e comparar o arquivo se esta corretamente.

---
+++ serial/memoriaSPI_Comparar.bat
@@ -0,0 +1,4 @@
+cd C:\Analog Devices\CrossCore Embedded Studio 2.10.0
+cldp -proc ADSP-BF592 -emu 1000 -driver "C:\Users\Ayalon\Documents\GitHub\Boot-ADSP-BF592\serial\Debug\bf592_m25p16_dpia.dxe" -cmd compare -format hex -file "C:\Users\Ayalon\Documents\GitHub\ESIG_SW_SPO2\Debug\ESIG_SW_SPO2.ldr"
+
+@pause
